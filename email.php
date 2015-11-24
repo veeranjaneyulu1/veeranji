09ee5fe6725b7acc7956b0f82958ae3b5d7b0bdb
veeranjaneyulu1 <veeranjaneyulu2909@gmail.com>
2015-11-24 19:03:17 +0530

    intial commit

---
+++ email.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body style="margin:0PX auto; background-color:#088bd7;">
+<div style="background:#FFF;height:100px;margin:0px auto;overflow:hidden;">
+<img src="zoom.png"style="float:left;padding:28px;"/>
+<h1 style="text-align:center;margin:0px;padding:28px;color:#088bd7;padding-right:295px; float:left; margin-left:290px;">Login Form</h1>
+<div style="float:right;margin-top:35px;margin-right:80px">
+<form method="post" name="login" action="login.php">
+
+<input type="text" name="emailid" placeholder="Enter your email" />
+<input type="password" name="Password" placeholder="password"  />
+<input type="submit" name="login" value="login" />
+</form>
+</div>
+<ul>
+<a href="forgot.php"><li style="float:right;margin-top:50px;margin-right:-280px;list-style-type:none; text-decoration:underline;">Forgot Password?</li></a>
+</ul>
+</div>
+
+</body>
+</html>
